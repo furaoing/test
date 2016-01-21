3ad6ad626a4d9f818a11e669ba9936f615b82290
furaoing <furaoing@hotmail.com>
2016-01-22 01:30:18 +0800

    add new test files

--- python/exception/catch_traceback.py
+++ python/exception/catch_traceback.py
@@ -2,7 +2,22 @@
 
 import traceback
 
+
+def my_readline(f_pth):
+    my_f = open(f_pth, "r")
+    while True:
+        s = my_f.readline()
+        if not s:
+            my_f.close()
+            break
+        yield s
+
+for x in my_readline("test"):
+    print(x)
+    print(len(x))
+
 if __name__ == "__main__":
+    """
     file_pth = "traceback.log"
     traceback_f = open(file_pth, "a")
     try:
@@ -11,3 +26,4 @@ if __name__ == "__main__":
         traceback.print_exc(100, traceback_f)
 
     print("The last statement")
+    """
